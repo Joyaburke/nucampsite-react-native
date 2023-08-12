82d81569acb94379e8ddf711088df50b084ad705
Joy Burke <joyaburke@gmail.com>
2023-08-12 12:58:47 -0700

    After workshop 8/12

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { ScrollView, Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { CAMPSITES } from '../shared/campsites';
 import { PROMOTIONS } from '../shared/promotions';
-import { PARTNERS } from '../shared/partners';
+import { PARTNERS } from '../shared/partners'
 
 const FeaturedItem = ({ item }) => {
     if (item) {
--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,10 +1,12 @@
 import { Platform, View } from 'react-native';
 import Constants from 'expo-constants';
 import CampsiteInfoScreen from './CampsiteInfoScreen';
+import HomeScreen from './HomeScreen';
 import DirectoryScreen from './DirectoryScreen';
+import ContactScreen from './ContactScreen';
+import AboutScreen from './AboutScreen';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import HomeScreen from './HomeScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -24,6 +26,32 @@ const HomeNavigator = () => {
             />
         </Stack.Navigator>
     );
+};
+
+const AboutNavigator = () => {
+    const Stack = createStackNavigator();
+        return (
+            <Stack.Navigator screenOptions={screenOptions}>
+                <Stack.Screen
+                    name='About'
+                    component={AboutScreen}
+                    options={{ title: 'About Us' }}
+                />
+            </Stack.Navigator>
+        );
+};
+
+const ContactNavigator = () => {
+    const Stack = createStackNavigator();
+        return (
+            <Stack.Navigator screenOptions={screenOptions}>
+                <Stack.Screen
+                    name='Contact'
+                    component={ContactScreen}
+                    options={{ title: 'Contact Us' }}
+                />
+            </Stack.Navigator>
+        );
 };
 
 const DirectoryNavigator = () => {
@@ -71,6 +99,16 @@ const Main = () => {
                     name='Directory'
                     component={DirectoryNavigator}
                     options={{ title: 'Directory' }}
+                />
+                <Drawer.Screen
+                    name='About'
+                    component={AboutNavigator}
+                    options={{ title: 'About' }}
+                />
+                <Drawer.Screen
+                    name='Contact'
+                    component={ContactNavigator}
+                    options={{ title: 'Contact' }}
                 />
             </Drawer.Navigator>
         </View>
