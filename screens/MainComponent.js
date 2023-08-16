c7f4638dc3ae626e8b8445578932ef2e8898ff05
Joy Burke <joyaburke@gmail.com>
2023-08-16 16:08:23 -0700

    Modal

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -11,8 +11,9 @@ import {
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
-import { Icon } from 'react-native-elements';
+import ReservationScreen from './ReservationScreen';
 import logo from '../assets/images/logo.png';
+import { Icon } from 'react-native-elements';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchPartners } from '../features/partners/partnersSlice';
@@ -94,6 +95,34 @@ const ContactNavigator = () => {
         </Stack.Navigator>
     );
 };
+
+
+const ReservationNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Reservation'
+                component={ReservationScreen}
+                options={({ navigation }) => ({
+                    title: 'Reservation Search',
+                    headerLeft: () => (
+                        <Icon
+                            name='tree'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
+
+
+
+
 
 const DirectoryNavigator = () => {
     const Stack = createStackNavigator();
@@ -197,6 +226,24 @@ const Main = () => {
                         )
                     }}
                 />
+
+              <Drawer.Screen
+                    name='ReserveCampsite'
+                    component={ReservationNavigator}
+                    options={{
+                        title: 'Reserve Campsite',
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name='tree'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+
                 <Drawer.Screen
                     name='About'
                     component={AboutNavigator}
