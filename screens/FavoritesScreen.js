e00f890bb065e69eeb63180cb521c2962f94788d
Joy Burke <joyaburke@gmail.com>
2023-08-21 10:14:28 -0700

    swip and alerts added

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,33 +1,63 @@
-import { useSelector } from "react-redux";
-import { View, FlatList, Text } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { View, FlatList, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
 import Loading from '../components/LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-import { exp } from "react-native-reanimated";
+import { SwipeRow } from "react-native-swipe-list-view";
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
+
+// import { exp } from "react-native-reanimated";
 
 const FavoritesScreen = ({ navigation }) => {
     const { campsitesArray, isLoading, errMess } = useSelector(
         (state) => state.campsites
     );
     const favorites = useSelector((state) => state.favorites);
+    const dispatach = useDispatch();
 
     const renderFavoriteItem = ({ item: campsite }) => {
         return (
-            <ListItem
-                onPress={() => navigation.navigate('Directory', {
-                    screen: 'CampsiteInfo',
-                    params: { campsite }
-                })
-            }
-            >
-                <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
-                <ListItem.Content>
-                    <ListItem.Title>{campsite.name}</ListItem.Title>
-                    <ListItem.Subtitle>
-                        {campsite.description}
-                    </ListItem.Subtitle>
-                </ListItem.Content>
-            </ListItem>
+            <SwipeRow rightOpenValue={-100}>
+                <View style={styles.deleteView}>
+                    <TouchableOpacity
+                        style={styles.deleteTouchable}
+                        onPress={() => Alert.alert ('Delete Favorite?', 
+                            'Are you sure you wish to delete the favorite campsite ' + campsite.name + '?', 
+                            [
+                                {
+                                    text: 'Cancel', 
+                                    onPress: () => console.log (campsite.name + 'Not Deleted'),
+                                    style: 'cancel'
+                                }, 
+                                {
+                                    text: 'OK',
+                                    onPress: () => dispatach(toggleFavorite(campsite.id))
+                                }
+                            ],
+                            { cancelable: false }
+                        )}
+                        >
+                            <Text style={styles.deleteText}>Delete</Text>
+                        </TouchableOpacity>
+                </View>
+                <View>
+                    <ListItem
+                        onPress={() => navigation.navigate('Directory', {
+                            screen: 'CampsiteInfo',
+                            params: { campsite }
+                        })
+                    }
+                    >
+                        <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
+                        <ListItem.Content>
+                            <ListItem.Title>{campsite.name}</ListItem.Title>
+                            <ListItem.Subtitle>
+                                {campsite.description}
+                            </ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                </View>
+            </SwipeRow>
         );
     };
 
@@ -50,6 +80,28 @@ const FavoritesScreen = ({ navigation }) => {
         />
     );
 };
+
+const styles = StyleSheet.create({
+    deleteView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: 'red', 
+        height: '100%',
+        justifyContent: 'center'
+    },
+    deleteText: {
+        color: 'white',
+        fontWeight: '700', 
+        textAlign: 'center',
+        fontSize: 16,
+        width:   100
+    }
+
+})
 
 
 export default FavoritesScreen;
