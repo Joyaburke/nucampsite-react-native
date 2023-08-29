ec1f917d26c1e2f99487a1f6678ea6f210ae3d71
Joy Burke <joyaburke@gmail.com>
2023-08-29 15:25:57 -0700

    added register and login tabs

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { View, Button, StyleSheet } from "react-native";
-import { CheckBox, Input } from "react-native-elements";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { CheckBox, Input, Button, Icon } from "react-native-elements";
 import *as SecureStore from 'expo-secure-store';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const LogicScreen = () => {
+const LoginTab = ({ navigation }) => {
     const [username, setUsername ] = useState('');
     const [password, setPassword ] = useState('');
     const [remember, setRemember ] = useState(false);
@@ -66,14 +67,90 @@ const LogicScreen = () => {
                     onPress={() => handleLogin()}
                     title='Login'
                     color='#5367DD'
+                    icon={
+                        <Icon 
+                            name='sign-in'
+                            type="font-awesom"
+                            color='#fff'
+                            iconStyle={{ marginRight: 10 }}
+                            />
+                    }
+                    buttonStyle={{ backgroundColor: '#5637DD' }}
+                />
+                </View>
+                <View style={styles.formButton}>
+                <Button 
+                    onPress={() => navigation.navigate('Register')}
+                    title='Register'
+                    type='clear'
+                    icon={
+                        <Icon 
+                            name='user-plus'
+                            type="font-awesome"
+                            color='blue'
+                            iconStyle={{ marginRight: 10 }}
+                            />
+                    }
+                    titleStyle={{ color: 'blue' }}
                 />
             </View>
         </View>
     );
 };
 
+const RegisterTab = () => {
+    return <ScrollView></ScrollView>;
+}
+
+const Tab = createBottomTabNavigator();
+
+const LoginScreen = () => {
+    const tabBarOptions = {
+        activeBackgroundColor: '#5637DD',
+        inactiveBackgroundColor: '#CEC8FF',
+        activeTintColor: '#fff',
+        inactiveTintColor: '#808080',
+        labelStyle: { fontSize: 16 }
+    };
+
+    return (
+        <Tab.Navigator tabBarOptions={tabBarOptions}>
+            <Tab.Screen
+                name='Login'
+                component={LoginTab}
+                options={{
+                    tabBarIcon: (props) => {
+                        return(
+                            <Icon
+                                name='sign-in'
+                                type='font-awesome'
+                                color={props.color}
+                            />
+                        )
+                    }
+                }}
+            />
+             <Tab.Screen
+                name='Register'
+                component={RegisterTab}
+                options={{
+                    tabBarIcon: (props) => {
+                        return(
+                            <Icon
+                                name='user-plus'
+                                type='font-awesome'
+                                color={props.color}
+                            />
+                        )
+                    }
+                }}
+             />
+        </Tab.Navigator>
+    )
+};
+
 const styles = StyleSheet.create({
-    container:{
+    container: {
         justifyContent: 'center',
         margin: 20
     },
@@ -93,5 +170,5 @@ const styles = StyleSheet.create({
 });
 
 
-export default LogicScreen;
+export default LoginScreen;
 
